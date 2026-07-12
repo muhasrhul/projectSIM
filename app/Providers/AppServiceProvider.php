ce8b2dfff3664aaec33a3e27104f2224cb4a8b8f
MUH.ASRUL <muhasrhull99@gmail.com>
2026-07-13 06:00:21 +0800

    fix: rename Dashboard ke Home di bottom nav

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -710,7 +710,7 @@ class AppServiceProvider extends ServiceProvider
                         nav.innerHTML = `
                             <a href="/admin" class="nav-home">
                                 <svg fill="none" viewBox="0 0 24 24" stroke-width="1.8"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955a1.126 1.126 0 011.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"/></svg>
-                                Dashboard
+                                Home
                             </a>
                             <a href="/admin/kasir-cepat-kantin" class="nav-kasir-link">
                                 <svg fill="none" viewBox="0 0 24 24" stroke-width="1.8"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75"/></svg>
