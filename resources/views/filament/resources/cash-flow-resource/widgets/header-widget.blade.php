6537e33988eb65361ed4da30a017dcd616db6877
MUH.ASRUL <muhasrhull99@gmail.com>
2026-07-15 18:37:57 +0800

    fix: rename header widget dari Laporan Arus Kas ke Analisis Laporan

--- resources/views/filament/resources/cash-flow-resource/widgets/header-widget.blade.php
+++ resources/views/filament/resources/cash-flow-resource/widgets/header-widget.blade.php
@@ -1,6 +1,6 @@
 <x-filament::widget>
     <div class="bg-gradient-to-r from-orange-50 to-orange-100 dark:from-gray-800 dark:to-gray-700 rounded-lg border border-orange-200 dark:border-gray-600 px-6 py-4">
-        <h1 class="text-xl font-bold text-gray-600 dark:text-gray-300 tracking-wide">LAPORAN ARUS KAS</h1>
+        <h1 class="text-xl font-bold text-gray-600 dark:text-gray-300 tracking-wide">ANALISIS LAPORAN</h1>
         <p class="text-sm text-gray-500 dark:text-gray-400 mt-1">{{ \Carbon\Carbon::now('Asia/Makassar')->translatedFormat('d F Y') }}</p>
     </div>
 </x-filament::widget>
